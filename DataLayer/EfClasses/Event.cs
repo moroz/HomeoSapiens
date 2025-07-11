f16f892f82d68cb804346f3a5b7076be4164d96e
Karol Moroz <karol@moroz.dev>
2025-07-11 22:17:01 +0800

    Define Event model

---
+++ DataLayer/EfClasses/Event.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace DataLayer.EfClasses;
+
+public class Event
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public DateTime BeginsAt { get; set; }
+    public DateTime EndsAt { get; set; }
+    public string Description { get; set; }
+    public DateTime InsertedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
--- DataLayer/EfCode/AppDbContext.cs
+++ DataLayer/EfCode/AppDbContext.cs
@@ -1,7 +1,9 @@
 using Microsoft.EntityFrameworkCore;
+using DataLayer.EfClasses;
 
 namespace DataLayer.EfCode;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
+    public DbSet<Event> Events { get; set; }
 }
