1ef7082cc7acd9b29f2c84d76fa3eee75d4a588f
Karol Moroz <karol@moroz.dev>
2025-07-11 22:05:26 +0800

    Create UUIDv7 generator function

---
+++ DataLayer/Migrations/20250711134558_CreateUUIDv7Function.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateUUIDv7Function : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            using var stream = assembly.GetManifestResourceStream("DataLayer.Migrations.CreateUUIDv7Function.sql");
+            if (stream == null)
+            {
+                throw new Exception($"Resource not found");
+            }
+
+            using var reader = new StreamReader(stream);
+            var sql = reader.ReadToEnd();
+            migrationBuilder.Sql(sql);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("drop function if exists uuid7();");
+            migrationBuilder.Sql("drop function if exists uuid7(timestamp with time zone);");
+            migrationBuilder.Sql("drop extension if exists pgcrypto;");
+        }
+    }
+}
